da1f255ce20e62c6cb9567c13d020c8f522a94c2
Krish Gupta <157712860+KrishGupta02@users.noreply.github.com>
2024-03-30 15:20:56 +0530

    Add files via upload

---
+++ Landing page/assets/js/main.js
@@ -0,0 +1,54 @@
+///////////////////////////////////////////////////////////
+
+
+const btnNav = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header-container");
+
+btnNav.addEventListener("click", function () {
+  header.classList.toggle("nav-open");
+});
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionElement = document.querySelector(href);
+      sectionElement.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+    if (link.classList.contains("nav-link")) {
+      header.classList.toggle("nav-open");
+    }
+  });
+});
+
+const sectionHero = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHero);
